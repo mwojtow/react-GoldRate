a5b258711e54bda5fc3820522be779b19778e0c0
Marcin Wojtowicz <marcin713@wp.pl>
2018-10-09 19:40:40 +0200

    min, max, median, date range, sort fix

--- src/App.js
+++ src/App.js
@@ -8,42 +8,26 @@ class App extends Component {
       dane: [],
       startDate: "",
       endDate: "",
+      maxDate: new Date().toJSON().slice(0, 10),
       sortedPrice: [],
       sortedDate: [],
-      toggle: false,
-      toggleDate: false
+      togglePrice: false,
+      toggleDate: false,
+      maxValue: "",
+      maxValueDate: "",
+      minValue: "",
+      minValueDate: "",
+      median: ""
     };
-    this.onChange = this.onChange.bind(this);
   }
-  onChange(e) {
+  onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
-  }
-
-  sortByPrice(arr) {
-    console.log("sort by price");
-    let { toggle } = this.state;
-    const sortedPrice = arr.sort((a, b) => {
-      return toggle ? b.cena > a.cena : b.cena < a.cena;
-    });
-    this.setState({ dane: sortedPrice, toggle: !toggle });
-  }
-
-  sortByDate(arr) {
-    let { toggleDate } = this.state;
-    const sortedDate = arr.sort((a, b) => {
-      console.log("sort data");
-      let c = new Date(a.data);
-      let d = new Date(b.data);
-      return toggleDate ? c > d : c < d;
-    });
-    this.setState({ dane: sortedDate, toggleDate: !toggleDate });
-  }
+  };
 
   getGoldRate = async e => {
     e.preventDefault();
 
-    const startDate = this.state.startDate;
-    const endDate = this.state.endDate;
+    const { startDate, endDate } = this.state;
 
     const api_call = await fetch(
       `http://api.nbp.pl/api/cenyzlota/${startDate}/${endDate}/`
@@ -51,46 +35,122 @@ class App extends Component {
 
     const data = await api_call.json();
 
-    this.setState({ dane: data });
+    const maxValue = Math.max(...data.map(o => o.cena));
+    const maxValueDate = data.find(o => {
+      return o.cena === maxValue;
+    });
+    const minValue = Math.min(...data.map(o => o.cena));
+    const minValueDate = data.find(o => {
+      return o.cena === minValue;
+    });
+
+    let median = function(array) {
+      array.sort(function(a, b) {
+        return a.cena - b.cena;
+      });
+      var mid = array.length / 2;
+      return mid % 1
+        ? array[mid - 0.5].cena
+        : (array[mid - 1].cena + array[mid].cena) / 2;
+    };
+
+    this.setState({
+      dane: data,
+      maxValue: maxValue,
+      minValue: minValue,
+      maxValueDate: maxValueDate.data,
+      minValueDate: minValueDate.data,
+      median: median(data)
+    });
+
+    console.log(this.state.median);
   };
+
+  sortByPrice(arr) {
+    let { togglePrice } = this.state;
+    const sortedPrice = arr.sort((a, b) => {
+      return togglePrice ? b.cena > a.cena : b.cena < a.cena;
+    });
+    this.setState({ dane: sortedPrice, togglePrice: !togglePrice });
+  }
+
+  sortByDate(arr) {
+    let { toggleDate } = this.state;
+    const sortedDate = arr.sort((a, b) => {
+      let c = new Date(a.data);
+      let d = new Date(b.data);
+      return toggleDate ? c > d : c < d;
+    });
+    this.setState({ dane: sortedDate, toggleDate: !toggleDate });
+  }
 
   render() {
     const { dane } = this.state;
     const mappedData = dane.map(data => {
       return (
-        <li>
-          DATA: {data.data}
-          CENA: {data.cena}
-        </li>
+        <tr>
+          <td>{data.data}</td>
+          <td>{data.cena}</td>
+        </tr>
       );
     });
 
     return (
       <div className="App">
         <header className="App-header">Gold rate</header>
-
+        <p>Wybierz zakres dat:</p>
         <input
           type="date"
           value={this.state.startDate}
+          max={this.state.maxDate}
           onChange={this.onChange}
           name="startDate"
         />
         <input
           type="date"
           value={this.state.endDate}
+          min={this.state.startDate}
+          max={this.state.maxDate}
           onChange={this.onChange}
           name="endDate"
         />
         <button onClick={this.getGoldRate}>Pobierz</button>
-        <p>{this.state.startDate}</p>
-        <p>{this.state.endDate}</p>
-        <button onClick={() => this.sortByDate(this.state.dane)}>
-          sort by date
-        </button>
-        <button onClick={() => this.sortByPrice(this.state.dane)}>
-          sort by price
-        </button>
-        <ul>{mappedData}</ul>
+
+        {this.state.dane.length !== 0 && (
+          <div>
+            <p>
+              <button onClick={() => this.sortByDate(this.state.dane)}>
+                sort by date
+              </button>
+              <button onClick={() => this.sortByPrice(this.state.dane)}>
+                sort by price
+              </button>
+            </p>
+            <table>
+              <thead>
+                <tr>
+                  <th>DATA</th>
+                  <th>CENA</th>
+                </tr>
+              </thead>
+              <tbody>{mappedData}</tbody>
+            </table>
+          </div>
+        )}
+
+        {this.state.maxValue && (
+          <p>
+            Największa wartość to: {this.state.maxValue} zł z dnia:{" "}
+            {this.state.maxValueDate}
+          </p>
+        )}
+        {this.state.minValue && (
+          <p>
+            Najmniejsza wartość to: {this.state.minValue} zł z dnia:{" "}
+            {this.state.minValueDate}
+          </p>
+        )}
+        {this.state.median && <p>Mediana to: {this.state.median} zł</p>}
       </div>
     );
   }
